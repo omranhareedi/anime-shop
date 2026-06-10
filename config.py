719e3faeb95356fc1869ac5b45fe3ca6326dcfc5
Anime Shop Student <student@anime-shop.edu>
2026-06-10 16:01:35 +0200

    chore: initial project scaffolding with Flask config, dependencies, and gitignore

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'anime-shop-secret-key-change-in-production')
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL',
+        'sqlite:///' + os.path.join(basedir, 'anime_shop.db')
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    WTF_CSRF_ENABLED = True
+    MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB max upload
